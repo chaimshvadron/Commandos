c36a1df109ec1ef33c855ebcd9e1158fc6a3be46
ChaimShvadron <s0527649958@gmail.com>
2025-05-27 11:26:19 +0300

    Fix Main method signature and enhance output for Commando instance

--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@
 {
     public class Program
     {
-        static public void Main(string[] arge)
+        public static void Main(string[] args)
         {
-
+            Commando commando = new Commando("John", "EagleEye");
+            commando.Name = "Jsdsdsd";
+            Console.WriteLine($"Commando Name: {commando.Name}");
+            Console.WriteLine($"Commando Code Name: {commando.CodeName}");
         }
     }
 }
