4772e0da4ad7df79551dd2798cdd8eb4da5a40ec
ChaimShvadron <s0527649958@gmail.com>
2025-05-28 11:04:11 +0300

    Refactor Weapon classes to implement M16 and AK47; enhance Stone class with breakable functionality

--- Weapon.cs
+++ Weapon.cs
@@ -21,8 +21,66 @@ namespace Commandos.Program
 
     }
 
-    class Stone
+    public class M16 : IWeapon, IShootable
     {
-        
+        public string Name { get; } = "M16";
+        public string Manufacturer { get; } = "COLT";
+        public int Bullets { get; set; } = 29;
+
+        public void Shoot()
+        {
+            if (Bullets > 0)
+            {
+                Bullets--;
+                Console.WriteLine($"{Name} shoots! Bullets left: {Bullets}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is out of bullets!");
+            }
+        }
     }
+
+    public class AK47 : IWeapon, IShootable
+    {
+        public string Name { get; } = "AK-47";
+        public string Manufacturer { get; } = "Kalashnikov";
+        public int Bullets { get; set; } = 30;
+
+        public void Shoot()
+        {
+            if (Bullets > 0)
+            {
+                Bullets--;
+                Console.WriteLine($"{Name} shoots! Bullets left: {Bullets}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is out of bullets!");
+            }
+        }
+    }
+
+    public class Stone : IWeapon, IBreakable
+    {
+        public string Name { get; } = "Stone";
+        public string Manufacturer { get; } = "Nature";
+        public string Status { get; set; } = "Intact";
+        public int MaxHits { get; } = 5;
+        public int CurrentHits { get; set; } = 0;
+
+        public void Hit()
+        {
+            CurrentHits++;
+            if (CurrentHits >= MaxHits)
+            {
+                Status = "Broken";
+            }
+            Console.WriteLine($"{Name} hit! Status: {Status} ({CurrentHits}/{MaxHits})");
+        }
+    }
+
+
+
+
 }
